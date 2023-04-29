a2bc154a5e49985594e4da8b06ccac17320f252b
treinado123 <raffoliveira@hotmail.com>
2023-04-29 01:27:26 +0000

    apache v.1.0

---
+++ script-iac2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "script resumido >>>>>>"
+
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
